15c73a2da7599255a82138f984f6a2f9fb4dd37e
Jarl André Hübenthal <jarl.andre@gmail.com>
2015-11-24 23:10:10 +0100

    Format

--- src/main/java/gfx/Texture.java
+++ src/main/java/gfx/Texture.java
@@ -9,39 +9,42 @@ import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
 
 public class Texture {
-    private final BufferedImage[] images;
-    private int currentIndex = 0;
-    
-    public Texture(String link) {
-        try {
-        	images = new BufferedImage[1];
-            images[0] = ImageIO.read(Texture.class.getClassLoader().getResourceAsStream(link));
-        } catch (IOException ex) {
-            throw new RuntimeException("Could not load texture " + link, ex);
-        }
-    }
-    
-    Runnable periodicTask = new Runnable() {
-        public void run() {
-        	if (currentIndex < images.length-1) {
-    			currentIndex ++;
-    		} else {
-    			currentIndex = 0;
-    		}
-        }
-    };
-    
-    public Texture(BufferedImage[] images) {
-    	this.images = images;
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        executor.scheduleAtFixedRate(periodicTask, 0, 500, TimeUnit.MILLISECONDS);
-    }
+	private final BufferedImage[] images;
+	private int currentIndex = 0;
+
+	public Texture(String link) {
+		try {
+			images = new BufferedImage[1];
+			images[0] = ImageIO.read(Texture.class.getClassLoader()
+					.getResourceAsStream(link));
+		} catch (IOException ex) {
+			throw new RuntimeException("Could not load texture " + link, ex);
+		}
+	}
+
+	Runnable periodicTask = new Runnable() {
+		public void run() {
+			if (currentIndex < images.length - 1) {
+				currentIndex++;
+			} else {
+				currentIndex = 0;
+			}
+		}
+	};
+
+	public Texture(BufferedImage[] images) {
+		this.images = images;
+		ScheduledExecutorService executor = Executors
+				.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(periodicTask, 0, 500,
+				TimeUnit.MILLISECONDS);
+	}
 
 	public BufferedImage[] getImages() {
 		return images;
 	}
-	
-	public BufferedImage getNext(){
+
+	public BufferedImage getNext() {
 		return images[currentIndex];
 	}
 }
